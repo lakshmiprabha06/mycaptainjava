522b3d3d501b7bd51c0edf6e3004aa07498f0f24
lakshmiprabha06 <77716144+lakshmiprabha06@users.noreply.github.com>
2021-01-20 14:13:34 +0530

    To print helloworld

--- helloworld.java
+++ helloworld.java
@@ -1,30 +1,10 @@
-public class classA {
-	int z=90;//instance variable
-	static int m=100;//static variable
+public class helloWorld {
 
 	public static void main(String[] args) {
-		classA obj=new classA();
+		System.out.println("Hello World");
 		// TODO Auto-generated method stub
-System.out.println("hello world");
-  obj.show();
-  obj.play();
+
 	}
 
-	
-	 void show()
-	{
-		int a=10;//local variable
-		a++;
-		m--;
-		System.out.println(a);
-		System.out.println(m);
-	}
-	
-	
-	void play()
-	{
-		int b=20;
-		z--;
-		
-	}
 }
+
