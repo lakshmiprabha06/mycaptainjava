d2da5671e70cf29e26f35cf57151cdc1ad9763aa
lakshmiprabha06 <77716144+lakshmiprabha06@users.noreply.github.com>
2021-01-20 11:49:49 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,29 @@
+public class classA {
+	int z=90;//instance variable
+	static int m=100;//static variable
+
+	public static void main(String[] args) {
+		classA obj=new classA();
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+  obj.show();
+  obj.play();
+	}
+
+	
+	 void show()
+	{
+		int a=10;//local variable
+		a++;
+		m--;
+		System.out.println(a);
+		System.out.println(m);
+	}
+	
+	
+	void play()
+	{
+		int b=20;
+		z--;
+	}
+}
